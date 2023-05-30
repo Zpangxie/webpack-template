ce64e6a34f5a8549d9cf6dd0f6017a5b0b72bc28
Zpangxie <870074823@qq.com>
2023-05-30 15:30:34 +0800

    chore: remove comment

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import './styles/index.css';
 function component() {
   const element = document.createElement('div');
   console.log(add(1, 2));
-
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash 在当前 script 中使用 import 引入
   element.innerHTML = join(['Hello', 'webpack'], ' ');
-
   return element;
 }
 
